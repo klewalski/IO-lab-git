d563101276148a9091e8253ce7144f60bf922b09
klewalski <klewalski@mat.umk.pl>
2025-11-29 09:17:35 +0000

    Dodanie funkcji czy_bezkwadratowa i wypisanie odpowiedzi

--- liczba.cpp
+++ liczba.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 
+bool czy_bezkwadratowa(int n){
+	if (n<= 0) return false;
+	int m = 2;
+	bool bezk = true;
+	while(m*m <= n){
+		if(n%(m*m) == 0) bezk = false;
+		m++;
+
+	}
+	return bezk;
+
+}
+
+
 int main(){
 	int n;
 	std::cin>>n;
-	std::cout<<n<<std::endl;
+	if(czy_bezkwadratowa(n)) std::cout<<n<<" "<<"liczba jest bezkwadratowa"<<std::endl;
+	else std::cout<<n<<" "<<"liczna nie jest bezkwadratowa"<<std::endl;
 	return 0;
 }
